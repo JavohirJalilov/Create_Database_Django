db6f57408ed2d83f623749643c29f7ee28f839c7
Jalilov <javohirjalilov777@gmail.com>
2021-12-26 18:18:04 +0500

    return __str__

--- App/models.py
+++ App/models.py
@@ -2,14 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    apple = models.CharField(max_length=50)
-    samsung = models.CharField(max_length=50)
-    xiomi = models.CharField(max_length=50)
-    huawei = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.category
 
 class Product(models.Model):
+    name = models.CharField(max_length=50)
     description = models.CharField(max_length=50)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
 class Product_options(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -18,9 +21,13 @@ class Product_options(models.Model):
     quantity = models.IntegerField()
     weight = models.FloatField()
     price = models.IntegerField()
+    def __str__(self):
+        return self.color
 
 class Specification(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     model = models.CharField(max_length=50)
     battery = models.CharField(max_length=50)
     camera = models.CharField(max_length=50)
+    def __str__(self):
+        return self.model
