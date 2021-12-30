6a79a6760465b5bcb9f356d083d112f65707c63a
Jalilov <javohirjalilov777@gmail.com>
2021-12-30 10:00:57 +0500

    admin panel show table

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Product, Product_options, Specification
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Product_options)
+admin.site.register(Specification)
